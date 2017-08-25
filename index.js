d732d43a55d665392d46a5d884b68e58949a8e7e
Daryl Concha <concha.daryl@gmail.com>
2017-08-25 18:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 function happyHolidays () {
-  console.log("Happy holidays!")
+  return("Happy holidays!")
 }
 function happyHolidaysTo (name) {
-  console.log(`Happy holidays, ${name}!`)
+  return(`Happy holidays, ${name}!`)
 }
 function happyHolidaysTo (holiday, name) {
-  console.log(`Happy ${holiday}, ${name}!`)
+  return(`Happy ${holiday}, ${name}!`)
 }
 function holidayCountdown (days, holiday) {
   return(`It's ${days} days until ${holiday}!`)
